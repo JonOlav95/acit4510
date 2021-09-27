32346054407a1a873daab672e9a719f226aa79e7
jo95holland <jo95holland@gmail.com>
2021-09-27 21:18:59 +0200

    Cast object values to integer

--- source/main.py
+++ source/main.py
@@ -12,8 +12,21 @@ def dataframe_to_dataset(dataframe):
     return ds
 
 
+def string_to_integer(dataframe):
+    for col in dataframe.columns:
+        data_type = dataframe[col].dtypes
+
+        if data_type == object:
+            unique = np.unique(dataframe[col].values)
+
+            for i in range(len(unique)):
+                dataframe[col].replace(unique[i], i, inplace=True)
+
+
 def split_data(dataframe):
+    dataframe.pop("id")
     dataframe = shuffle(dataframe)
+    string_to_integer(dataframe)
 
     df_true = dataframe[dataframe["stroke"] == 1]
     df_false = dataframe[dataframe["stroke"] == 0]
@@ -39,9 +52,12 @@ def main():
     dataframe = pd.read_csv("dataset.csv")
     train_data, test_data, val_data = split_data(dataframe)
 
+    print(train_data.describe().transpose()[["mean", "std"]])
+
+
 
 if __name__ == '__main__':
-    physical_devices = tf.config.list_physical_devices("GPU")
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    #physical_devices = tf.config.list_physical_devices("GPU")
+    #tf.config.experimental.set_memory_growth(physical_devices[0], True)
     main()
 
